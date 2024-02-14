679ab5aa29f26c38b2dc0e86251c5bf149021157
GAURAV KUMAR <gauravkumar22927@gmail.com>
2024-02-15 00:45:29 +0530

    Update server.js cors error correction

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ const app = express()
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
 })
 
 app.use("/images", express.static(path.join(__dirname, "images")))
